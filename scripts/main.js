859b74f8487f6fd36a1dacaf2951d47ddb2de5c0
Michael Haufe <tno@thenewobjective.com>
2020-06-23 13:44:59 -0500

    header and nav styling updates

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,7 @@ import {qsa, qs} from './query.js'
 
 let galleries = qsa('.gallery').map(item => new Gallery({ el:item })),
     comments = new Comments({el: qs('.comments')})
+
+let activeItem = qs('.site-nav_item[data-active]')
+if(activeItem)
+    activeItem.scrollIntoView()
