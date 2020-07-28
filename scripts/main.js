2a08f685f61bcca2f91a4f0cb32b5342aa26a2d1
Michael Haufe <tno@thenewobjective.com>
2020-07-28 13:47:47 -0500

    Updated site layout for better mobile display

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,3 @@ import {qsa, qs} from './query.js'
 
 let galleries = qsa('.gallery').map(item => new Gallery({ el:item })),
     comments = new Comments({el: qs('.comments')})
-
-let activeItem = qs('.site-nav_item[data-active]')
-if(activeItem)
-    activeItem.scrollIntoView()
