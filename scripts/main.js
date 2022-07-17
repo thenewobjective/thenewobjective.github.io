e2bb45177a159234f1db2432f6a73cd31f8e177d
Michael Haufe <tno@thenewobjective.com>
2022-07-17 12:03:26 -0500

    hash link fix

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,15 @@ if(qs('.mermaid')) {
     });
 }
 
+// This Chrome bug seems to still exist as of 2022-07-17
+// https://stackoverflow.com/a/38588927/153209
+(() => {
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if (window.location.hash && isChrome) {
+        setTimeout(function () {
+            var hash = window.location.hash;
+            window.location.hash = "";
+            window.location.hash = hash;
+        }, 300);
+    }
+})();
