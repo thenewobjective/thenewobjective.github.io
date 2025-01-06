96047ddb6fa1954b94ee25e704a1aeb0f0f1378d
Michael Haufe <tno@thenewobjective.com>
2025-01-05 19:47:32 -0600

    Update giscus script category to Announcements

--- components/Comments.vue
+++ components/Comments.vue
@@ -8,8 +8,8 @@ const { title } = useData()
     <div :key="title" class="giscus">
         <component :is="'script'" src="https://giscus.app/client.js"
             data-repo="thenewobjective/thenewobjective.github.io" data-repo-id="MDEwOlJlcG9zaXRvcnkyNTY4OTk0MjY="
-            data-category="General" data-category-id="DIC_kwDOD0_5Ys4CW9Wa" data-mapping="pathname" data-strict="0"
-            data-reactions-enabled="1" data-emit-metadata="0" data-input-position="top" data-lang="en"
+            data-category="Announcements" data-category-id="DIC_kwDOD0_5Ys4CW9Wa" data-mapping="pathname"
+            data-strict="0" data-reactions-enabled="1" data-emit-metadata="0" data-input-position="top" data-lang="en"
             data-theme="transparent_dark" data-loading="lazy" crossorigin="anonymous" async />
     </div>
 </template>
